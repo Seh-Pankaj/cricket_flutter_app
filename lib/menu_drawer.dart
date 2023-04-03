604a9781849334e3030168eda33dcab11c767556
Devashish <humantch56@gmail.com>
2023-04-03 21:03:50 +0530

    Dependencies added

---
+++ lib/menu_drawer.dart
@@ -0,0 +1,67 @@
+import 'package:cricket_flutter_app/info_screen.dart';
+import 'package:cricket_flutter_app/movie_reviews.dart';
+import 'package:cricket_flutter_app/weather_screen.dart';
+import 'package:flutter/material.dart';
+
+class MenuDrawer extends StatelessWidget {
+  const MenuDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: buildMenuItems(context),
+      ),
+    );
+  }
+
+  List<Widget> buildMenuItems(BuildContext ctx) {
+    final List<String> menuTitles = [
+      'Weather',
+    ];
+
+    List<Widget> menuItems = [];
+
+    menuItems.add(
+      const DrawerHeader(
+        decoration: BoxDecoration(color: Colors.amberAccent),
+        child: Center(
+          child: Text(
+            'IMDB Pro',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30,
+            ),
+          ),
+        ),
+      ),
+    );
+
+    menuTitles.forEach((element) {
+      Widget screen = Container(); // it holds an empty container
+      menuItems.add(ListTile(
+        title: Text(
+          element,
+          style: const TextStyle(fontSize: 22),
+        ),
+        onTap: () {
+          switch (element) {
+            case 'Home':
+              screen = InfoScreen();
+              break;
+            case 'Movie Review':
+              screen = MovieReview();
+              break;
+
+            case 'Weather':
+              screen = WeatherScreen();
+          }
+          //Navigator.of(ctx).pop();
+          Navigator.of(ctx).push(MaterialPageRoute(builder: (ctx) => screen));
+        },
+      ));
+    });
+
+    return menuItems;
+  }
+}
